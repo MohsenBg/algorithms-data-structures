a2ad4a723b4db826d4b89ff1f0ce78ebcee8e785
Bagheri <moh.1380.1393@gmail.com>
2022-07-18 17:36:58 +0430

    Sorting

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -126,4 +126,32 @@ class Sorting
 			delete[] leftSide;
 			delete[] rightSide;
 		}
+
+		static void SelectionSort(int * array , int size)
+		{
+			for(int i = 0; i < size; i++)
+			{
+				int min = i;
+				for(int j=i+1; j < size; j++)
+				{
+					if(array[min] > array[j])
+						min = j;
+				}
+				swap(array[min],array[i]);
+			}
+		}
+		static void InsertionSort(int *array, int size)
+		{
+			for(int i = 1; i < size; i++)
+			{
+				int current = array[i];
+				int j = i-1;
+				while(j >=0 && array[j] > current)
+				{
+					array[j+1] = array[j];
+					j--;
+				}		
+				array[j+1] = current;
+			}
+		}
 };
